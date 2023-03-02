b062ea635bfb1bcfef0c8281b3e87bad95c20bf0
Uladzislau Dzeika <deiko640@gmail.com>
2023-03-02 21:48:46 +0100

    Add pages Login and Register

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Add from "../assets/add.png"
+
+export const Login = () => {
+  return (
+    <div className="formContainer">
+        <div className="formWrapper">
+            <span className="logo">My Chat</span>
+            <span className="title">Login</span>
+            <form action="">
+                <input type="email" placeholder='email'/>
+                <input type="password" placeholder='password'/>
+                <button>Sign in</button>
+            </form>
+            <p>You don`t have an account? Register</p>
+        </div>
+    </div>
+  )
+}
